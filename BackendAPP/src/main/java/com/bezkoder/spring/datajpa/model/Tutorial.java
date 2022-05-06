f2b1ee89c44a05571c8240f7ff513f0096cae067
stivensrinconmoreno <stivensrinconmoreno@gmail.com>
2022-05-05 22:33:50 -0500

    backend y frontend

---
+++ BackendAPP/src/main/java/com/bezkoder/spring/datajpa/model/Tutorial.java
@@ -0,0 +1,93 @@
+package com.bezkoder.spring.datajpa.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tutorials")
+public class Tutorial {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	@Column(name = "title")
+	private String title;
+
+	@Column(name = "description")
+	private String description;
+
+	@Column(name = "published")
+	private boolean published;
+	
+	@Column(name = "cancha")
+	private String cancha;
+	
+	@Column(name = "hora")
+	private String hora;
+
+	public Tutorial() {
+
+	}
+
+	public Tutorial( String title, String description, String cancha, String hora, boolean published) {
+		this.title = title;
+		this.description = description;
+		this.cancha = cancha;
+		this.hora = hora;
+		this.published = published;
+		
+		
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public String getCancha() {
+		return cancha;
+	}
+
+	public void setCancha(String cancha) {
+		this.cancha = cancha;
+	}
+	
+	public String getHora() {
+		return hora;
+	}
+
+	public void setHora(String hora) {
+		this.hora = hora;
+	}
+
+	public boolean isPublished() {
+		return published;
+	}
+
+	public void setPublished(boolean isPublished) {
+		this.published = isPublished;
+	}
+	
+
+
+	@Override
+	public String toString() {
+		return "Tutorial [id=" + id + ", title=" + title + ", desc=" + description +", cancha=" + cancha+ ", hora=" + hora +", published=" + published +"]";
+	}
+
+}
